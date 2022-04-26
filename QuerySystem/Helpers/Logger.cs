989fdf60f41035601f6bb797562ff1d74cc8a42f
aei541579 <aei541579@gmail.com>
2022-04-26 21:56:00 +0800

    處理logger無法存取檔案問題

--- QuerySystem/Helpers/Logger.cs
+++ QuerySystem/Helpers/Logger.cs
@@ -11,15 +11,16 @@ namespace QuerySystem.Helpers
         private const string _savePath = "C:\\log\\log.log";
         public static void WriteLog(string moduleName, Exception ex)
         {
-            string content = $@"---------\r\n
-                                {DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}
-                                {moduleName}
-                                {ex.ToString()}
-                                ---------\r\n
-                                {Environment.NewLine}";
+            string content = $"---------\r\n{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}\r\n{moduleName}\r\n{ex.ToString()}\r\n---------\r\n{Environment.NewLine}";
+
+            if (!Directory.Exists("C:\\log"))
+                Directory.CreateDirectory("C:\\log");
             if (!File.Exists(_savePath))
-                File.Create(_savePath);
-            File.AppendAllText(_savePath, content);
+                File.Create(_savePath).Close();
+            
+                File.AppendAllText(_savePath, content);
+            
+
         }
     }
 }
